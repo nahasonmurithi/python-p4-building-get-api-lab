f62b594046966b87b261a837b29a89a1dbbd1ac8
Nahason <nahason.murithi@student.moringaschool.com>
2024-01-11 23:15:12 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -20,19 +20,36 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakeries.append(bakery.to_dict())
+    response = make_response(jsonify(bakeries), 200)
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query,filter(Bakery.id==id).first()
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(jsonify(bakery_dict), 200)
+    response.header["Content-Type"] = "application/json"
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = []
+    for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all():
+        baked_goods.append(baked_good.to_dict())
+    response = make_response(jsonify(baked_goods), 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    bakedGood_dict = baked_good.to_dict()
+    response = make_response(jsonify(bakedGood_dict), 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
